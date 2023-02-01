9f5d443ea310e2d3d398b34d0db9662151cd822c
FernandoTomasCenturion <ferc721@gmail.com>
2023-02-01 06:40:06 -0300

    agrego práctica

---
+++ WindowsClinica/Profesor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsClinica
+{
+    public class Profesor
+    {
+        public int ProfesorID { get; set;}
+        public string Nombre { get; set;}
+        public string Apellido { get; set; }
+
+    }
+}
